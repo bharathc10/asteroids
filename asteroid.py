6f6ffa4beec0691c9de66dff27c7b113f94f7b21
bharathc10 <cb987579@gmail.com>
2026-01-15 19:09:22 +0530

    added asteroid splitting when hit

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame, random
 from circleshape import CircleShape
 from constants import *
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,5 +12,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
-
     
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        split_angle = random.uniform(20, 50)
+        new_direction_left = self.velocity.rotate(split_angle)
+        new_direction_right = self.velocity.rotate(-split_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_left = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_right = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid_left.velocity = new_direction_left * 1.2
+        asteroid_right.velocity = new_direction_right * 1.2
